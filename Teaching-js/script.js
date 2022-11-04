f0833a8366776d139c52774e4bc8c1ad8bcc9774
JitinKGoyal <jkgoyal85@gmail.com>
2022-11-04 13:45:59 +0530

    array

---
+++ Teaching-js/script.js
@@ -0,0 +1,28 @@
+// let a = Math.floor(Math.random() * 100);
+
+// console.log(a);
+
+// console.log(Math.floor(25.5656))
+
+// // OVER
+
+// console.log("outer")
+
+// let r1 = Math.floor(Math.random() * 255)
+// let r2 = Math.floor(Math.random() * 255)
+// let r3 = Math.floor(Math.random() * 255)
+
+// console.log(`rgb(${r1}, ${r2}, ${r3})`)
+
+// setTimeout(() => {
+
+//     document.getElementById('bd').style.background = `rgb(${r1}, ${r2}, ${r3})`;
+//     document.getElementById('bd').innerHTML = 'After 2 sec'
+
+// }, 2000);
+
+// setTimeout(() => {
+    
+// }, timeout);
+
+// // OVER
