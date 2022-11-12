d843f2b91c71a874453cf439898f5b1d11c1b5cc
JitinKGoyal <jkgoyal85@gmail.com>
2022-11-12 14:26:34 +0530

    props

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -4,17 +4,48 @@ import Navbar from './Navbar';
 
 function App() {
 
+  let mData = {
+    myName: "mohit",
+    school: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam non tempore quos ad aperiam? Ad voluptas facere iusto eius molestias incidunt dolorem animi aspernatur quam. Iste assumenda ad perspiciatis ipsum unde at blanditiis veritatis earum. Similique, illo doloribus! Alias tempora quasi praesentium tempore facere at.",
+    class: "mohit class",
+    imgUrl: "https://www.gowallpaper.co.uk/media/catalog/product/cache/a35feaac9df764e89b33b339cfadd4b6/5/9/59119_kailani_dark_blue_roomset.jpg"
+  }
+
+  let pData = {
+    myName: "pawan",
+    school: "pawan school",
+    class: "pawan class",
+    imgUrl: "https://img.freepik.com/free-photo/cool-geometric-triangular-figure-neon-laser-light-great-backgrounds-wallpapers_181624-9331.jpg?w=2000"
+  }
+
+  let sData = {
+    myName: "sandeep",
+    school: "sandeep school",
+    class: "sandeep class",
+    imgUrl: "https://media.istockphoto.com/id/1257005098/photo/hd-wallpaper-for-mobile-phones.jpg?b=1&s=170667a&w=0&k=20&c=Hl1nOy1NbJbEvGQsoAW4zDtMmNg9yPL7H7a1mdftly8=" 
+  }
+
+  let rData = {
+    myName: "ritesh",
+    school: "ritesh school",
+    class: "ritesh class",
+    imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp9REClTzT7YmgnXrXgrfgDw3xLV673aPq4Q&usqp=CAU"
+  }
+
+
   return (
     <>
 
       <Navbar />
 
-      <div className="d-flex">
+      <div className="d-flex justify-content-center">
 
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card war={rData} />
+        <Card war={mData} />
+        <Card war={pData} />
+        <Card war={sData} />
+
+
 
       </div>
 
--- React/first-app/src/Card.js
+++ React/first-app/src/Card.js
@@ -1,12 +1,20 @@
 
-function Card() {
+function Card(e) {
+
+    // console.log(e.war.class);
+
     return (
         <>
             <div className="card" style={{ width: '18rem' }}>
-                <img src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg" className="card-img-top" alt="..." />
+
+                <div style={{ height: "250px", overflow: "auto" }}>
+                    <img src={e.war.imgUrl} className="card-img-top" alt="..." />
+                </div>
+
                 <div className="card-body">
-                    <h5 className="card-title">Jitin</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 className="card-title">{e.war.myName}</h5>
+                    <p className="card-text">{e.war.class}</p>
+                    <p className="card-text">{e.war.school}</p>
                     <a href="/" className="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
