5d541a9db17bed962056c4244aff1124b5b4d902
JitinKGoyal <jkgoyal85@gmail.com>
2022-12-16 18:37:52 +0530

    refactor

--- React/img-editor/src/components/ImagePreview.js
+++ React/img-editor/src/components/ImagePreview.js
@@ -6,48 +6,44 @@ function ImagePreview(props) {
   const [images, setImage] = useState([]);
   const [first, setFirst] = useState(1);
 
+
+  // Function to add an image to "images" Array and sessionStorage.
   function handleImage(e) {
     let arr = images;
     arr.push(URL.createObjectURL(e.target.files[0]));
-
     setImage(arr);
     setFirst(first + 1);
 
     sessionStorage.setItem("imagesArray", JSON.stringify(arr));
   }
 
-  const forDeleteImage = (i, e) => {
 
+  // Funtion to delete image from sessionStorage.
+  const forDeleteImage = (i, e) => {
     e.stopPropagation();
 
     let sessionArray = JSON.parse(sessionStorage.getItem("imagesArray"));
     sessionArray.splice(i, 1);
     sessionStorage.setItem("imagesArray", JSON.stringify(sessionArray));
-    
     setFirst(first + 1);
-
   }
 
+
+  // useEffect for first mount of component.
   useEffect(() => {
-
     if (sessionStorage.getItem("imagesArray")) {
-
       let sessionArray = JSON.parse(sessionStorage.getItem("imagesArray"));
       setImage(sessionArray);
-   
     }
-
   }, [])
 
+
+  // useEffect when "first" state updates.
   useEffect(() => {
-
     if (sessionStorage.getItem("imagesArray")) {
-
       let sessionArray = JSON.parse(sessionStorage.getItem("imagesArray"));
       setImage(sessionArray);
-    
     }
-
   }, [first])
 
 
@@ -60,7 +56,6 @@ function ImagePreview(props) {
         <div>
           <hr />
           <div className='d-flex flex-wrap'>
-
             {images.map((e, i) => (
 
               <div key={i} onClick={() => props.setSelectedImage(e)} className='d-flex justify-content-center shadow-lg p-4 imgPreview'>
@@ -69,10 +64,9 @@ function ImagePreview(props) {
                   <i className="fa fa-trash" aria-hidden="true"></i>
                 </div>
               </div>
-
+              
             ))}
           </div>
-
         </div>
       }
 
