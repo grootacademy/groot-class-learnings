5ea5e488f2ed8462b24a436902c3bbc0da912298
JitinKGoyal <jkgoyal85@gmail.com>
2022-12-26 16:27:07 +0530

    Initial version of laventaEcommerce generated by generator-jhipster@7.9.3

---
+++ laventa-e-commerce/backend-java/src/main/java/com/laventa/ecommerce/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.laventa.ecommerce.domain;
---
+++ laventa-e-commerce/backend-java/src/main/java/com/laventa/ecommerce/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.laventa.ecommerce.web.rest.vm;
