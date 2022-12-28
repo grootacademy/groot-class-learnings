b42a38582282eb6a373d544458191bb8f2b9114e
JitinKGoyal <jkgoyal85@gmail.com>
2022-12-28 12:36:51 +0530

    Initial version of laventaEcommerce generated by generator-jhipster@7.9.3

---
+++ laventa-e-commerce/for-mohit/src/main/java/com/laventa/ecommerce/domain/Category.java
@@ -0,0 +1,130 @@
+package com.laventa.ecommerce.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * The Category entity.\n@author A true hipster
+ */
+@Schema(description = "The Category entity.\n@author A true hipster")
+@Entity
+@Table(name = "category")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    @Column(name = "id")
+    private Long id;
+
+    /**
+     * name
+     */
+    @Schema(description = "name", required = true)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+    @JsonIgnoreProperties(value = { "category" }, allowSetters = true)
+    private Set<Product> products = new HashSet<>();
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Category id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Category name(String name) {
+        this.setName(name);
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return this.products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        if (this.products != null) {
+            this.products.forEach(i -> i.setCategory(null));
+        }
+        if (products != null) {
+            products.forEach(i -> i.setCategory(this));
+        }
+        this.products = products;
+    }
+
+    public Category products(Set<Product> products) {
+        this.setProducts(products);
+        return this;
+    }
+
+    public Category addProduct(Product product) {
+        this.products.add(product);
+        product.setCategory(this);
+        return this;
+    }
+
+    public Category removeProduct(Product product) {
+        this.products.remove(product);
+        product.setCategory(null);
+        return this;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Category)) {
+            return false;
+        }
+        return id != null && id.equals(((Category) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Category{" +
+            "id=" + getId() +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+}
